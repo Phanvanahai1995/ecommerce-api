64b5c38bbb1e27b07f3b48d7d613be7686217b2b
phanvanhai <phanvanhaixdddk49@gmail.com>
2024-08-14 17:05:14 +0700

    first commit

--- middleware/cors.js
+++ middleware/cors.js
@@ -3,12 +3,18 @@ require("dotenv").config();
 const whitelist = [process.env.CORS_URL, "More url"];
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
+
+  // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200,
+
+  // CORS sẽ cho phép nhận cookies từ request
+  credentials: true,
 };
 
 module.exports = corsOptions;
